8e221333b45b127d5461f40500d317e31efff3ef
jhoyosubarnes <jose.pb2017@gmail.com>
2021-09-09 22:07:37 -0500

    final-proyect

---
+++ index.js
@@ -0,0 +1,35 @@
+const path = require('path');
+const express = require('express');
+const app = express();
+const socket = require('socket.io');
+
+
+//configuraciones
+app.set('port', process.env.PORT || 3000);
+
+// Archivos estaticos
+app.use(express.static(path.join(__dirname, "public")));
+
+// arrancar el servidor
+const server = app.listen(app.get('port'), () => {
+  console.log('Server on port', app.get('port'));
+});
+
+
+// WebSockets
+const io = socket(server);
+io.on('connection', (socket) => {
+  console.log('socket connection opened:', socket.id);
+  
+  socket.on('chat:message', function(data) {
+    io.sockets.emit('chat:message', data);
+  });
+
+  socket.on('chat:typing', function(data) {
+    socket.broadcast.emit('chat:typing', data);
+  });
+});
+
+
+
+
---
+++ src/app/rooms.js
@@ -0,0 +1,4 @@
+// var app = require('express')();
+// var http = require('http').createServer(app);
+// var io = require('socket.io')(http);
+// const shortid = require('shortid');
